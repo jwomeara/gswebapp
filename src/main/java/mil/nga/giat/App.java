54d2afc02236d79bc4e8fa16fa7c6403ee2b6b49
Whitney O'Meara <womeara@radiantblue.com>
2017-04-14 10:42:39 -0400

    Initial Commit

---
+++ src/main/java/mil/nga/giat/App.java
@@ -0,0 +1,13 @@
+package mil.nga.giat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
